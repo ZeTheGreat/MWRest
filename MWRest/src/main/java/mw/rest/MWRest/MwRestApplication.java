dd9fe5aee8eb591eaf9297ff883caa0c06eedd22
ZeTheGreat <32179394+ZeTheGreat@users.noreply.github.com>
2019-03-26 12:10:50 -0300

    Initial commit
    Esqueleto do projeto.

---
+++ MWRest/src/main/java/mw/rest/MWRest/MwRestApplication.java
@@ -0,0 +1,13 @@
+package mw.rest.MWRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwRestApplication.class, args);
+	}
+
+}
