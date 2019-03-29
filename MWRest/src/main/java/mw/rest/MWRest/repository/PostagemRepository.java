558e9940faa54feb004752a1b1f06e2c6d70b879
alexislopes <alexisdrops@gmail.com>
2019-03-29 20:24:10 -0300

    Criação dos Repositories.

---
+++ MWRest/src/main/java/mw/rest/MWRest/repository/PostagemRepository.java
@@ -0,0 +1,8 @@
+package mw.rest.MWRest.repository;
+
+import mw.rest.MWRest.model.Postagem;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostagemRepository extends CrudRepository<Postagem, Long> {
+
+}
---
+++ MWRest/src/main/java/mw/rest/MWRest/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package mw.rest.MWRest.repository;
+
+import mw.rest.MWRest.model.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+}
