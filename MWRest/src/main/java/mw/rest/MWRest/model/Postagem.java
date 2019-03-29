5859d1f286ac6b465d876918881a3b4bdcfe2a07
alexislopes <alexisdrops@gmail.com>
2019-03-29 20:18:06 -0300

    término da criação dos modelos e criação do banco de dados.

--- MWRest/src/main/java/mw/rest/MWRest/model/Postagem.java
+++ MWRest/src/main/java/mw/rest/MWRest/model/Postagem.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "POSTAGEM")
@@ -12,9 +14,21 @@ public class Postagem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "POST_ID")
-    @Getter
-    @Setter
+    @Getter @Setter
     private Long id;
+
+    @Column(name = "POST_TITULO", length = 140, nullable = false)
+    @Getter @Setter
+    private String titulo;
+
+    @Column(name = "POST_CONTEUDO")
+    @Getter @Setter
+    private String conteudo;
+
+    @ManyToMany(mappedBy = "postagens", cascade = CascadeType.ALL)
+    @Getter @Setter
+    private List<Usuario> usuarios = new ArrayList<>();
+
 
 
 
--- MWRest/src/main/java/mw/rest/MWRest/model/Usuario.java
+++ MWRest/src/main/java/mw/rest/MWRest/model/Usuario.java
@@ -14,23 +14,29 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USR_ID")
-    @Getter @Setter private Long id;
+    @Getter @Setter
+    private Long id;
 
     @Column(name = "USR_NOME", length = 20, nullable = false)
-    @Getter @Setter private String nome;
+    @Getter @Setter
+    private String nome;
 
     @Column(name = "USR_USUARIO", unique = true, length = 20, nullable = false)
-    @Getter @Setter private String usuario;
+    @Getter @Setter
+    private String usuario;
 
     @Column(name = "USR_SENHA", length = 20, nullable = false)
-    @Getter @Setter private String senha;
+    @Getter @Setter
+    private String senha;
 
-    @Column(name = "USR_SENHA", length = 20, nullable = false)
-    @Getter @Setter private String email;
+    @Column(name = "USR_EMAIL", length = 20, nullable = false)
+    @Getter @Setter
+    private String email;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "POSTAGEM_USUARIO",
     joinColumns = {@JoinColumn(name = "USR_ID")},
     inverseJoinColumns = {@JoinColumn(name = "POST_ID")})
-    @Getter @Setter private List<Postagem> postagens;
+    @Getter @Setter
+    private List<Postagem> postagens;
 }
